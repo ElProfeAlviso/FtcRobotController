2c24d627703a18da4c41e2ad4cb5c235b8361178
ElProfeAlviso <alejandro.alviso@tec.mx>
2025-01-16 11:23:39 -0600

    Autonomo con mecanismos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoIzquierdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoIzquierdo.java
@@ -9,10 +9,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -23,16 +19,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Autonomous(name="Auto Moves", group="Autos",preselectTeleOp="TitaniumRamsRegional")
 
 //Clase principal del programa (IMU utilizado modelo:
-public class MovimientosAutos extends LinearOpMode {
+public class AutoIzquierdo extends LinearOpMode {
 
     // Declarar los objetos de motores
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
-    private DcMotor arm = null;
-    private DcMotor wrist = null;
-    private Servo claw = null;
-    private CRServo intake = null;
-
     private BHI260IMU imu;
     private Orientation angulos;
     private DistanceSensor distanceSensor;
@@ -45,84 +36,18 @@ public class MovimientosAutos extends LinearOpMode {
     // Define el encoder ticks por revolution (TPR) del motor
     private static final int TICKS_PER_REV =  420;//yMark NeveRest 40
 
-    // Arm and Wrist target positions for each state
-    //Posiciones de Arm en diferentes configuraciones.
-    private static final int ARM_POSITION_INIT = 170;
-    private static final int ARM_POSITION_INTAKE = 300;
-    private static final int ARM_POSITION_WALL_GRAB = 920;
-    private static final int ARM_POSITION_WALL_UNHOOK = 1270;
-    private static final int ARM_POSITION_HOVER_HIGH = 1950;
-    private static final int ARM_POSITION_CLIP_HIGH = 1500;
-    private static final int ARM_POSITION_LOW_BASKET = 1870;
-
-    //Posiciones de WRIST en diferentes configuraciones.
-    private static final int WRIST_POSITION_INIT = 0;
-    private static final int WRIST_POSITION_SAMPLE = 230;
-    private static final int WRIST_POSITION_SPEC = 10;
-    private static final int WRIST_POSITION_BASKET = 285;
-
-
-
-    // Posiciones de CLAW en diferentes configuraciones.
-    private static final double CLAW_OPEN_POSITION = 0.50;
-    private static final double CLAW_CLOSED_POSITION = 0.65;
-
-    // ENUM para asignación de los diferentes estados del robot.
-    private enum RobotState {
-        INIT,
-        INTAKE,
-        WALL_GRAB,
-        WALL_UNHOOK,
-        HOVER_HIGH,
-        CLIP_HIGH,
-        LOW_BASKET,
-        MANUAL
-    }
-
-    // Enviar robot a posición inicial.
-    private RobotState currentState = RobotState.INIT;
-
-    // CLAW toggle state.
-    private boolean clawOpen = true;
-    private boolean lastBump = false;
-    private boolean lastHook = false;
-    private boolean lastGrab = false;
-
-    //target position de arm y Wrist.
-    private int targetArm = 0;
-    private int targetWrist = 0;
-
     @Override
     public void runOpMode() {
         // inicializa las variables de hardware, los strings son los nombres de parametros en la driver station.
         // La funcion get obtiene el mapeo de la configuracion de hardware en el driver station.
         leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        wrist = hardwareMap.get(DcMotor.class, "wrist");
-        claw = hardwareMap.get(Servo.class, "claw");
-        intake = hardwareMap.get(CRServo.class, "intake");
-
-
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        wrist.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        // Set motors to use encoders
-        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Se establece la direccion de los motores.Debido a que los motores estan invertidos uno respeco al otro,
         // se invierte el motor derecho.
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        wrist.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         //Se establece el modo de freno al estar en neutral.
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -156,51 +81,6 @@ public class MovimientosAutos extends LinearOpMode {
 
         waitForStart();
 
-        switch (currentState) {
-            case INIT:
-                targetArm = ARM_POSITION_INIT;
-                targetWrist = WRIST_POSITION_INIT;
-                telemetry.addData("State", "INIT");
-                break;
-            case INTAKE:
-                targetArm = ARM_POSITION_INTAKE;
-                targetWrist = WRIST_POSITION_SAMPLE;
-                telemetry.addData("State", "INTAKE");
-                break;
-
-            case WALL_GRAB:
-                targetArm = ARM_POSITION_WALL_GRAB;
-                targetWrist = WRIST_POSITION_SPEC;
-                telemetry.addData("State", "WALL_GRAB");
-                break;
-
-            case WALL_UNHOOK:
-                targetArm = ARM_POSITION_WALL_UNHOOK;
-                targetWrist = WRIST_POSITION_SPEC;
-                telemetry.addData("State", "WALL_UNHOOK");
-                break;
-
-            case HOVER_HIGH:
-                targetArm = ARM_POSITION_HOVER_HIGH;
-                targetWrist = WRIST_POSITION_SPEC;
-                telemetry.addData("State", "HOVER_HIGH");
-                break;
-
-            case CLIP_HIGH:
-                targetArm = ARM_POSITION_CLIP_HIGH;
-                targetWrist = WRIST_POSITION_SPEC;
-                telemetry.addData("State", "CLIP_HIGH");
-                break;
-            case LOW_BASKET:
-                targetArm = ARM_POSITION_LOW_BASKET;
-                targetWrist = WRIST_POSITION_BASKET;
-                telemetry.addData("State", "LOW_BASKET");
-                break;
-            case MANUAL:
-                telemetry.addData("State", "MANUAL");
-                break;
-        }
-
         //===================SECUENCIA DE COMANDOS AUTONOMOS====================================
 
         Adelante(30,0.6,100);
@@ -227,23 +107,10 @@ public class MovimientosAutos extends LinearOpMode {
 
 
 
-        arm.setTargetPosition(targetArm);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        wrist.setTargetPosition(targetWrist);
-        wrist.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        arm.setPower(1);
-        wrist.setPower(1);
-
-        // Send telemetry data to the driver station
-        telemetry.addData("Claw Position", clawOpen ? "Open" : "Closed");
-        telemetry.addData("Arm Position", arm.getCurrentPosition());
-        telemetry.addData("Arm Power", arm.getPower());
-        telemetry.addData("Wrist Position", wrist.getCurrentPosition());
-        telemetry.addData("Wrist Power", wrist.getPower());
-        telemetry.update();
 
 
-        //================================FUNCIONES DE INTAKE=================================
+
+
 
 
 
@@ -371,36 +238,10 @@ public class MovimientosAutos extends LinearOpMode {
             telemetry.update();
         }
 
-
         leftDrive.setPower(0);
         rightDrive.setPower(0);
 
         sleep(SLEEPTIME);
-    }
-
-    private void AbrirGarra(){
-
-        claw.setPosition(CLAW_OPEN_POSITION);
-
-    }
-
-    private void CerrarGarra() {
-
-        claw.setPosition(CLAW_CLOSED_POSITION);
-    }
-
-    private void IntakeMeter (){
-        intake.setPower(1.0);
-
-    }
-
-    private void IntakeSacar(){
-
-        intake.setPower(-1.0);
-    }
-
-    private void IntakeStop (){
-        intake.setPower(0);
     }
 }
 
